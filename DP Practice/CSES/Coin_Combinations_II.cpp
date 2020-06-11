2219a76897af42e5f60a83245754211fd4bf5124
Akash Tadwai <akashadarsh.tadwai@gmail.com>
2020-06-11 20:52:04 +0530

    modified comments in Coin_Combinations_II

--- DP Practice/CSES/Coin_Combinations_II.cpp
+++ DP Practice/CSES/Coin_Combinations_II.cpp
@@ -24,17 +24,23 @@ void init() {
   cin >> n >> x;
   vi a(n);
   fr(i, 0, n) cin >> a[i];
-  /* vector<vector<int> >dp(n+1,vector<int>(x+1)); // dp[i]- no of ways from
- first i coins with sum i dp[0][0]=1; for(int i=1;i<=n;i++){ for(int
- j=0;j<=x;j++){ dp[i][j]=dp[i-1][j]; if(j-a[i-1]>=0)
+  /* vector<vector<int> >dp(n+1,vector<int>(x+1)); // dp[i][j] - no of ways from first i coins with sum exactly j
+   dp[0][0]=1;
+  for(int i=1;i<=n;i++){
+     for(int j=0;j<=x;j++){
+        dp[i][j]=dp[i-1][j];
+         if(j-a[i-1]>=0)
  (dp[i][j]+=dp[i][j-a[i-1]])%=mod;
        }
    }
  cout<<dp[n][x]<<endl;*/
   vector<int> dp(x + 1); // dp[i] no of ways with coins exactly i
   for (int i = 0; i < n; i++) {
-    for (int j = a[i]; j <= x; j++) { // we are iterating left to right coz we need to update the values of dp[j] every time 
-      dp[j] += dp[j - a[i]]; // For more info see @qqibro's answer , https://stackoverflow.com/questions/27880842/space-optimized-solution-for-coin-change
+    for (int j = a[i]; j <= x;
+         j++) { // we are iterating left to right coz we need to update the
+                // values of dp[j] every time
+      dp[j] += dp[j -a[i]]; // For more info see @qqibro's answer ,
+                  // https://stackoverflow.com/questions/27880842/space-optimized-solution-for-coin-change
     }
   }
   cout << dp[x] << endl;
